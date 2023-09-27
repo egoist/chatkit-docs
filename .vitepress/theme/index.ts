ecbe673911f87006baf9e1dd94e41089d69bcc4d
EGOIST <hi@egoist.dev>
2023-09-27 18:13:07 +0800

    ai chat

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,18 +1,20 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from "vue";
-import Theme from "vitepress/theme";
-import VideoPlayer from "../components/VideoPlayer.vue";
-import "./tailwind.css";
-import "./style.css";
+import { h } from "vue"
+import Theme from "vitepress/theme"
+import VideoPlayer from "../components/VideoPlayer.vue"
+import InjectKenja from "../components/InjectKenja.vue"
+import "./tailwind.css"
+import "./style.css"
 
 export default {
   ...Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    });
+      "layout-top": () => h(InjectKenja),
+    })
   },
   enhanceApp({ app, router, siteData }) {
-    app.component("VideoPlayer", VideoPlayer);
+    app.component("VideoPlayer", VideoPlayer)
   },
-};
+}
