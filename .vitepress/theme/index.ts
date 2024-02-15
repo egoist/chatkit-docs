e85d0df63edd154dcdd0164183fad94c16bd0bea
EGOIST <hi@egoist.dev>
2024-02-15 19:40:55 +0800

    zoom image

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,18 +1,40 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from "vue"
+import { defineComponent, h, nextTick, onMounted, watch } from "vue"
+import { useRoute } from "vitepress"
 import Theme from "vitepress/theme"
+import mediumZoom from "medium-zoom"
+
 import VideoPlayer from "../components/VideoPlayer.vue"
 import "./tailwind.css"
 import "./style.css"
 
+const initZoom = () => {
+  mediumZoom(".content img")
+}
+
 export default {
   ...Theme,
 
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout: defineComponent({
+    setup() {
+      const route = useRoute()
+      onMounted(() => {
+        initZoom()
+      })
+
+      watch(
+        () => route.path,
+        () => {
+          nextTick(() => initZoom())
+        }
+      )
+
+      return () =>
+        h(Theme.Layout, null, {
+          // https://vitepress.dev/guide/extending-default-theme#layout-slots
+        })
+    },
+  }),
   enhanceApp({ app, router, siteData }) {
     app.component("VideoPlayer", VideoPlayer)
   },
